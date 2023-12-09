c2ed324e5a0bda7bf806f9cab185f16ed9993a3a
Rares Cruceat <cruceat.rares@gmail.com>
2023-12-09 14:30:12 +0200

    update home page with new centered app bar and new strings

--- app/src/main/java/com/crxapplications/jetpackcomposesandbox/flows/home/ui/page/HomePage.kt
+++ app/src/main/java/com/crxapplications/jetpackcomposesandbox/flows/home/ui/page/HomePage.kt
@@ -2,16 +2,25 @@ package com.crxapplications.jetpackcomposesandbox.flows.home.ui.page
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.crxapplications.jetpackcomposesandbox.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage() {
-    Scaffold { padding ->
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = {
+            Text(stringResource(id = R.string.home_title))
+        })
+    }) { padding ->
         Column(modifier = Modifier.padding(padding)) {
-            Text("Home Page")
+            Text("WIP")
         }
     }
 }
